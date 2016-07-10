5d0675df8dd477f7c4fa297ceecbc22db6681163
PoroshkinaVV <lerik-erik@pochta.ru>
2016-07-10 13:05:32 +0300

    painting buildings

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,22 @@
 
 b = Building.create(name: "k")
 bg = BuildingGeometry.create(building_id: b.id, b_coordinates_array: [[0,0], [210,0],[210,210],[0,210]])
+b2 = Building.create(name: "g")
+bg2 = BuildingGeometry.create(building_id: b2.id, b_coordinates_array: [[0,0], [210,0],[210,210],[0,210]])
 
 a = Auditory.create(name: "k-1", building_id: b.id)
 ag = AuditoryGeometry.create(auditory_id: a.id, a_coordinates_array: [[10,10], [110,10],[110,110],[10,110]])
+a2 = Auditory.create(name: "g-1", building_id: b.id)
+ag2 = AuditoryGeometry.create(auditory_id: a2.id, a_coordinates_array: [[120,120], [200,120],[200,200],[120,200]])
+a3 = Auditory.create(name: "g-2", building_id: b2.id)
+ag3 = AuditoryGeometry.create(auditory_id: a3.id, a_coordinates_array: [[10,10], [110,10],[110,110],[10,110]])
 
 d = Desk.create(name: "d1", auditory_id: a.id)
 dg = DeskGeometry.create(front_point: 1, desk_id: d.id, d_coordinates_array: [[50,20], [80,20],[80,50],[50,50]])
+d2 = Desk.create(name: "d1", auditory_id: a3.id)
+dg2 = DeskGeometry.create(front_point: 1, desk_id: d2.id, d_coordinates_array: [[50,20], [80,20],[80,50],[50,50]])
 
 c = Computer.create(name: "1", desk_id: d.id)
-cg = ComputerGeometry.create(front_point: 1, computer_id: 1, c_coordinates_array: [[52,30], [60,30],[60,40],[52,40]])
+cg = ComputerGeometry.create(front_point: 1, computer_id: c.id, c_coordinates_array: [[52,30], [60,30],[60,40],[52,40]])
+c2 = Computer.create(name: "1", desk_id: d2.id)
+cg2 = ComputerGeometry.create(front_point: 1, computer_id: c2.id, c_coordinates_array: [[52,30], [60,30],[60,40],[52,40]])
